4481298bb2bc4a1eeaf3bfcf10859905f04be303
Tarun7386 <nellikudurutarun1234@gmail.com>
2024-10-03 00:25:41 +0530

    added vercel.json

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.use('/deactivateUser',deactivateRouter)
 app.use('/message',sendMessageRouter)
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000,function(){
   console.log('Server started on port 3000');
