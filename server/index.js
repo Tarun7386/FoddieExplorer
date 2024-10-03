60761e89f61b7190a8546d617504f6b59a2ecdae
Tarun7386 <nellikudurutarun1234@gmail.com>
2024-10-03 19:32:46 +0530

    done

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.use('/deactivateUser',deactivateRouter)
 app.use('/message',sendMessageRouter)
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'src')));
+// app.use(express.static(path.join(__dirname, 'src')));
 
 // app.listen(3000,function(){
 //   console.log('Server started on port 3000');
