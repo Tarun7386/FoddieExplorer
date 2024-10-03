b47bad861f46daaf6e0e5a7fadbc16ec5f94f078
Tarun7386 <nellikudurutarun1234@gmail.com>
2024-10-03 19:22:24 +0530

    done

--- server/index.js
+++ server/index.js
@@ -50,7 +50,8 @@ app.use('/message',sendMessageRouter)
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'src')));
 
-app.listen(3000,function(){
-  console.log('Server started on port 3000');
-})
+// app.listen(3000,function(){
+//   console.log('Server started on port 3000');
+// })
 
+module.exports = app;
